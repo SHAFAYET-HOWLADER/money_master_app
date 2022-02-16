09effefba8ecabe4d6c5dce792875b836bebeff4
unknown <you@example.com>
2022-02-17 02:18:56 +0600

    get remaining value

--- js/app.js
+++ js/app.js
@@ -27,7 +27,7 @@ document.getElementById("calculate-btn").addEventListener("click",function(){
     const totalExpensesValue = parseFloat(totalExpensesText);
     totalExpenses.innerText = Spend;
 
-    //remaining money
+    //remaining balance
     const extraMoney = salaryTotal - Spend;
     const remainingBalance = document.getElementById("balance");
     const remainingBalanceText = remainingBalance.innerText;
@@ -35,7 +35,68 @@ document.getElementById("calculate-btn").addEventListener("click",function(){
     remainingBalance.innerText = extraMoney;
 })
 
+    // get save button
+    document.getElementById("save-button").addEventListener("click", function(){
+    // get save input value
+    const saveText = document.getElementById("save-input");
+    const saveTextValue = saveText.value;
+    const saveTextTotalValue = parseFloat(saveTextValue);
 
+   //get salary-input
+    const salary = document.getElementById("salary-input");
+    const salaryText = salary.value;
+    const salaryTotal = parseFloat(salaryText);
+
+    //saving amount
+    const savingAmount = salaryTotal / saveTextValue;
+    saveText.value = savingAmount;
+    
+    //set saving amount innerText
+    const savingAmountText = document.getElementById("saving-amount");
+    const savingAmountTotal =  savingAmountText.innerText;
+    const savingAmountTotalValue = parseFloat(savingAmountTotal);
+    savingAmountText.innerText = savingAmount;
+
+
+//re-use calculate button
+  //get foot-input
+  const food = document.getElementById("food");
+  const foodText = food.value;
+  const foodTotal = parseFloat(foodText);
+   
+  //get rent-input
+  const rent = document.getElementById("rent");
+  const rentText = rent.value;
+  const rentTotal = parseFloat(rentText);
+  
+  //get rent-input
+  const clothes = document.getElementById("clothes");
+  const clothesText = clothes.value;
+  const clothesTotal = parseFloat(clothesText);
+  
+  //total spend from salary
+  const Spend = foodTotal + rentTotal +clothesTotal;
+  const totalExpenses = document.getElementById("total-spend");
+  const totalExpensesText = totalExpenses.innerText;
+  const totalExpensesValue = parseFloat(totalExpensesText);
+  totalExpenses.innerText = Spend;
+
+  //remaining balance
+  const extraMoney = salaryTotal - Spend;
+  const remainingBalance = document.getElementById("balance");
+  const remainingBalanceText = remainingBalance.innerText;
+  const remainingTotal = parseFloat(remainingBalanceText);
+  remainingBalance.innerText = extraMoney;
+//re-use calculate button
+
+    // get remaining balance after saving
+    const finalRemainingAmount = extraMoney - savingAmount;
+    //get remaining balance
+    const remainingAmountText = document.getElementById("remaining-amount");
+    const remainingAmountTextValue = remainingAmountText.innerText;
+    const remainingAmountTextValueTotal = parseFloat(remainingAmountTextValue);
+    remainingAmountText.innerText = finalRemainingAmount;
+})
 
 
 
